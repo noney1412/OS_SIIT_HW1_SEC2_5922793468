e4c7a61d48a7e36caa2172d60efdfcf18e3c5b03
noney1412 <nonpanpila@gmail.com>
2018-02-21 21:55:34 +0700

    :tada: Done 1 !

--- Child/a.c
+++ Child/a.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main() { printf("Test 1234"); }
+// Negative Value: creation of a child process was unsuccessful.
+// Zero: Returned to the newly created child process.
+// Positive value: Returned to parent or caller. The value contains process ID
+// of newly created child process.
+
+int main() {
+
+  pid_t pid_A;
+
+  pid_A = fork();
+
+  printf("Hello world! process_ID %d \n", getpid());
+
+  if (getpid() < 0) {
+    printf("error")
+  }
+
+  return 0;
+}
